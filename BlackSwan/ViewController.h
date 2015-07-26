6bbe37a49e16cf28ea9da4adb7a8557922f4bd8b
Robin Spinks <uglybluecat@googlemail.com>
2015-07-26 13:04:28 +0100

    Initial Commit

---
+++ BlackSwan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlackSwan
+//
+//  Created by Robin Spinks on 26/07/2015.
+//  Copyright (c) 2015 Robin Spinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlackSwan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlackSwan
+//
+//  Created by Robin Spinks on 26/07/2015.
+//  Copyright (c) 2015 Robin Spinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
